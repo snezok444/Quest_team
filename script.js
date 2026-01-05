befc2bde006950ffe5dbc2bd7d5d09f4703ee612
snezok444 <7797739@gmail.com>
2026-01-06 02:14:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
-const WEBAPP_URL = "https://script.google.com/macros/s/AKfycbwROhi0tc652Ev3a-p-IKzC4idCWbshYsMjXF-q3G6xfo32A_CED9lhpzpyX-Apos5k/exec";
+const WEBAPP_URL = "https://script.google.com/macros/s/ТВОЙ_ID/exec";
 
 async function submitBooking(event) {
-  event.preventDefault(); // предотвращаем перезагрузку страницы
+  event.preventDefault();
 
-  // Получаем значения из формы
   const name = document.getElementById("name").value.trim();
   const phone = document.getElementById("phone").value.trim();
   const telegram = document.getElementById("telegram").value.trim();
   const time = document.getElementById("time").value;
 
-  const url = $WEBAPP_URL?name=$encodeURIComponent(name)&phone=$encodeURIComponent(phone)&telegram=$encodeURIComponent(telegram)&time=$encodeURIComponent(time);
+  const url = `${WEBAPP_URL}?name=${encodeURIComponent(name)}&phone=${encodeURIComponent(phone)}&telegram=${encodeURIComponent(telegram)}&time=${encodeURIComponent(time)}`;
 
   try {
-    // Отправляем запрос
     const response = await fetch(url);
     const result = await response.json();
 
-    // Обрабатываем ответ
     if (result.ok) {
       document.getElementById("bookingForm").style.display = "none";
       document.getElementById("status").innerText = "✅ Время успешно забронировано!";
